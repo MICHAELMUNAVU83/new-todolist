574b8d5cafda27c97ac9a8dc3f6f1667ec74a972
trance <michaelmunavu83@gmail.com>
2022-06-01 06:55:47 +0300

    Pushed actual code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,39 @@
 import './styles/main.css';
-import lala from './joke.js';
+import tasks from './joke.js';
 
-console.log(lala);
+const heading = document.querySelector('.heading');
+
+heading.innerHTML = `
+  <div class="headingDiv">
+                  <p>Today's To Do</p>
+                  <i class="fa-solid fa-arrows-rotate"></i>
+                
+              </div>
+              
+              <div class="inputdiv">
+                  <input id="taskinput" type="text" placeholder="Add to your list">
+                
+                  <i class="fa-solid fa-arrow-right-to-bracket"></i>
+  
+              </div>
+  
+  
+  `;
+
+tasks.forEach((task) => {
+  const holdingtask = document.createElement('div');
+  holdingtask.classList.add('eachtask');
+  holdingtask.innerHTML += `
+      
+                  <input type="checkbox" name="name">${task.description}<i
+                          class="fa-solid fa-ellipsis-vertical"></i>
+          
+      
+      `;
+  document.getElementById('tasklist').appendChild(holdingtask);
+});
+document.querySelector('.clear').innerHTML = `
+  <button class="clearall">Clear all complted</button>
+  
+  
+  `;
--- src/joke.js
+++ src/joke.js
@@ -1,3 +1,14 @@
-const lala = 123;
+const tasks = [
+  {
+    description: 'wash the dishes',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'complete to do list',
+    completed: true,
+    index: 2,
+  },
+];
 
-export default lala;
+export default tasks;
