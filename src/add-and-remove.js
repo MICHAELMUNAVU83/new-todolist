d95f05144451fccb18774de29091dc2a34653ebd
trance <michaelmunavu83@gmail.com>
2022-06-02 13:16:36 +0300

    pushed final code

--- src/add-and-remove.js
+++ src/add-and-remove.js
@@ -1,21 +1,21 @@
 const storedtasks = [];
 class TaskBook {
   constructor(taskname, completed, index) {
-    this.taskname = taskname,
-    this.completed = completed,
-    this.index = index,
+    (this.taskname = taskname),
+      (this.completed = completed),
+      (this.index = index);
     this.id = Date.now();
   }
 }
 
-const addl = document.getElementById('taskadd');
+const addl = document.getElementById("taskadd");
 
-addl.addEventListener('click', () => {
-  const taskel = document.getElementById('taskinput').value;
+addl.addEventListener("click", () => {
+  const taskel = document.getElementById("taskinput").value;
   const newtask = new TaskBook(taskel, false, storedtasks.length);
-  const holdingtask = document.createElement('div');
+  const holdingtask = document.createElement("div");
 
-  holdingtask.classList.add('eachtask');
+  holdingtask.classList.add("eachtask");
   holdingtask.innerHTML = `
   <input class="inputcheck" type="checkbox"><p class= "taskp" contenteditable="true">${newtask.taskname}</p>
   <div>
@@ -24,28 +24,26 @@ addl.addEventListener('click', () => {
   </div>
 
 
-     
-                     
       
       `;
-  document.getElementById('tasklist').appendChild(holdingtask);
+  document.getElementById("tasklist").appendChild(holdingtask);
   storedtasks.push(newtask);
-  localStorage.setItem('tasks', JSON.stringify(storedtasks));
-  document.getElementById('taskinput').value = '';
+  localStorage.setItem("tasks", JSON.stringify(storedtasks));
+  document.getElementById("taskinput").value = "";
   function changeContent() {
-    const taskp = document.querySelectorAll('.taskp');
+    const taskp = document.querySelectorAll(".taskp");
 
     taskp.forEach((item) => {
       let previous = item.textContent;
-      item.addEventListener('keydown', (e) => {
-        if (e.key === 'Enter') {
+      item.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") {
           e.preventDefault();
           const newcontent = item.textContent;
           console.log(newcontent);
           const bla = storedtasks.filter((todo) => todo.taskname === previous);
           storedtasks[bla[0].index].taskname = newcontent;
           previous = newcontent;
-          localStorage.setItem('tasks', JSON.stringify(storedtasks));
+          localStorage.setItem("tasks", JSON.stringify(storedtasks));
         }
       });
     });
@@ -53,13 +51,13 @@ addl.addEventListener('click', () => {
   changeContent();
 });
 
-document.getElementById('tasklist').addEventListener('click', (e) => {
-  if (e.target.innerHTML === 'del') {
+document.getElementById("tasklist").addEventListener("click", (e) => {
+  if (e.target.innerHTML === "del") {
     e.target.parentElement.parentElement.remove();
     storedtasks.forEach((storedtask, i) => {
       if (
         e.target.parentElement.lastElementChild.classList.contains(
-          storedtask.id,
+          storedtask.id
         )
       ) {
         storedtasks.splice(i, 1);
@@ -68,19 +66,19 @@ document.getElementById('tasklist').addEventListener('click', (e) => {
         storedtasks[i].index = i;
       }
 
-      localStorage.setItem('tasks', JSON.stringify(storedtasks));
+      localStorage.setItem("tasks", JSON.stringify(storedtasks));
     });
   }
 
   e.preventDefault();
 });
 
-window.addEventListener('load', () => {
-  const preservedTasks = JSON.parse(localStorage.getItem('tasks'));
+window.addEventListener("load", () => {
+  const preservedTasks = JSON.parse(localStorage.getItem("tasks"));
   preservedTasks.forEach((preservedTask) => {
-    const holdingtask = document.createElement('div');
+    const holdingtask = document.createElement("div");
 
-    holdingtask.classList.add('eachtask');
+    holdingtask.classList.add("eachtask");
     holdingtask.innerHTML = `
     <input class="inputcheck" type="checkbox"><p class= "taskp" contenteditable="true">${preservedTask.taskname}</p>
     <div>
@@ -93,6 +91,6 @@ window.addEventListener('load', () => {
                        
         
         `;
-    document.getElementById('tasklist').appendChild(holdingtask);
+    document.getElementById("tasklist").appendChild(holdingtask);
   });
 });
